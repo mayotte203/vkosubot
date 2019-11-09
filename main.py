a83314804b6710dc2a331cb849cea73cdb624bfe
Egor Romanovskiy, 750505 <37875413+mayotte203@users.noreply.github.com>
2019-11-09 18:29:39 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from bot.bot import bot
-from osu.api import OSUApi
-vk_token = "529521db9f05c9176672d8ceedfd01cb503a179ada3af15b3bba5693d1b0a0eabc1bedcfc2e5ff1bb7b83"
-osu_token = "3541ef6f05c8814ed2396befc4dded47f09f104e"
-
-osu_bot = bot(vk_token)
-osu_bot.run(osu_token)
-
